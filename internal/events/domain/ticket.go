32193480530e9e41a2838ecd82f24606a9d0893a
Paulo Benatto <paulobenatto02@gmail.com>
2024-06-24 21:59:52 -0300

    feat: Add event repository interface and spot service
    This commit adds the event repository interface and spot service to the events domain package. The event repository interface defines methods for listing events, finding events by ID, finding spots by event ID, finding spots by name, and reserving a spot. The spot service provides a method for generating spots for an event based on a given quantity. It also includes error handling for invalid quantities.

--- internal/events/domain/ticket.go
+++ internal/events/domain/ticket.go
@@ -1,6 +1,13 @@
 package domain
 
+import "errors"
+
 type TicketType string
+
+var (
+	ErrTicketPriceZero = errors.New("ticket price must be greater than zero")
+) 
+
 
 const (
 	TicketTypeHalf TicketType = "half"
@@ -14,3 +21,22 @@ type Ticket struct {
 	TicketType TicketType
 	Price      float64
 }
+
+func IsValidTicketType(ticketType TicketType) bool {
+	return ticketType == TicketTypeHalf || ticketType == TicketTypeFull
+}
+
+func (t *Ticket) CalculatePrice() {
+	if (t.TicketType == TicketTypeHalf) {
+		t.Price /= 2
+	}
+}
+
+func (t *Ticket) Validate() error {
+	if t.Price <= 0 {
+		return ErrTicketPriceZero
+	}
+
+	return nil
+}
+
